06f0301253cf8d7773a4ec65f0a55990abd68be6
colteo <colazzomatteo@gmail.com>
2022-05-07 11:55:07 +0200

    Aggiunti instrument in InstrumentType

--- Domain/Enum/instrument_type.py
+++ Domain/Enum/instrument_type.py
@@ -4,6 +4,10 @@ from enum import Enum
 class InstrumentType(Enum):
     eurusd = 'EUR_USD'
     eurchf = 'EUR_CHF'
+    eurgbp = 'EUR_GBP'
+    euraud = 'EUR_AUD'
+    eurcad = 'EUR_CAD'
+    eurnzd = 'EUR_NZD'
 
     def __str__(self):
         return self.value
